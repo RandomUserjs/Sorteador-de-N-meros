8ab330b472b2d7a78a45cf9fc015a7b0c2ec2a03
RandomUser.js <arthurbarrosramos7@gmail.com>
2024-03-14 11:02:23 -0300

    Update 1.1

--- app.js
+++ app.js
@@ -1,44 +1,56 @@
 // Variáveis Globais
+let quantidade = null;
+let de = null;
+let ate = null;
 let numerosSorteados = [];
+
+let botaoReiniciar = document.getElementById("btn-reiniciar")
+botaoReiniciar.setAttribute("disabled", true);
 
 // Função que sorteia os números escolhidos pelo usuário
 function sortear() {
     numerosSorteados = []
-    let quantidade = document.getElementById("quantidade").value;
-    let de = document.getElementById("de").value;
-    let ate = document.getElementById("ate").value;
-    if (quantidade == "" || de == "" || ate == "") 
-    {
-        alert("Preencha todos os campos!");
+    quantidade = parseInt(document.getElementById("quantidade").value);
+    de = parseInt(document.getElementById("de").value);
+    ate = parseInt(document.getElementById("ate").value);
+
+    if (isNaN(quantidade) || isNaN(de) || isNaN(ate)) {
+        alert("Preencha todos os campos corretamente!");
+        quantidade = 0;
+        de = 0;
+        ate = 0;
+        document.getElementById("quantidade").value = " ";
+        document.getElementById("de").value = " ";
+        document.getElementById("ate").value = " ";
         return;
     }
-    if (quantidade > 10)
-    {
+
+    if (quantidade > 10) {
         alert("Que tal escolher uma quantidade de números menor?");
         quantidade = 10;
         document.getElementById("quantidade").value = quantidade;
         return;
     }
-    
-    
-    if (quantidade <= 0) 
-    {
-       alert("A quantidade de números deve ser maior que 0!");
+
+
+    if (quantidade <= 0) {
+        alert("A quantidade de números deve ser maior que 0!");
+        quantidade = 1;
+        document.getElementById("quantidade").value = quantidade;
         return;
     }
-    if (de >= ate)
-    {
+    if (de >= ate) {
         alert("O número máximo não pode ser igual ou menor que o mínimo!");
+        ate = de + 3;
+        document.getElementById("ate").value = ate;
         return;
     }
     for (let i = 1; i <= quantidade; i++) {
         let randomNumber = parseInt(Math.random() * ate + de);
-        if (randomNumber >= de && randomNumber <= ate) 
-        {
+        if (randomNumber >= de && randomNumber <= ate) {
             numerosSorteados.push(randomNumber);
         }
-        else 
-        {
+        else {
             i--;
         }
 
@@ -46,7 +58,13 @@ function sortear() {
     let texto = document.getElementsByClassName("texto__paragrafo")[3];
     texto.innerHTML = `Números sorteados: ${numerosSorteados}`;
     console.log(numerosSorteados);
-    let botaoReiniciar = document.getElementById("btn-reiniciar")
-    botaoReiniciar.setAttribute("disabled", false);
+
+    botaoReiniciar.removeAttribute("disabled");
 }
 
+function reiniciar() {
+    let texto = document.getElementsByClassName("texto__paragrafo")[3];
+    texto.innerHTML = "Números sorteados:  nenhum até agora";
+    let botaoReiniciar = document.getElementById("btn-reiniciar")
+    botaoReiniciar.setAttribute("disabled", true)
+}
